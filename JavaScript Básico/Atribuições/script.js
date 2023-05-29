523327cdd054c6d2dcdf15f545c763d1eb47ff23
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 15:15:51 -0300

    Usando atribuições na prática

---
+++ JavaScript Básico/Atribuições/script.js
@@ -0,0 +1,11 @@
+// += Atribuição com o número da direita
+// -= 
+// *=
+// /=
+// %=
+// A vantagem de usar essas atribuições é que quando queremos fazer mudanças com valores fixos temos uma maneira mais fácil
+
+var a = 25
+a += 5 
+
+console.log(a)
