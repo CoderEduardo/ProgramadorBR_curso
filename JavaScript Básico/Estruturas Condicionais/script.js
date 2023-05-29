7172d01d993f1411990c74138869c353747136eb
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 18:11:41 -0300

    Usando if e else na prática

---
+++ JavaScript Básico/Estruturas Condicionais/script.js
@@ -0,0 +1,18 @@
+var idade = 18
+
+if (idade >= 18) {
+    console.log("Mostre a identidade")
+} else if (idade >= 35) {
+    console.log("Pode beber")
+    console.log("Qual é o seu pedido?")
+}
+else {
+    console.log("Não pode beber")
+    console.log("Volte Futuramente")
+}
+
+/*
+Para criar condições usamos o if e else
+
+Se atender a primeira condição o código dentro do if roda, e se não atender ele cai para o segundo bloco do else if, e assim sucessivamente
+*/
