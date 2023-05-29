c3c0a1b9d71ff11ea601cbf8e6cf41e882014620
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 15:53:16 -0300

    Operadores Comparativos na prática

---
+++ JavaScript Básico/Operadores Comparativos/script.js
@@ -0,0 +1,14 @@
+// == Igual a 
+// === Idêntico a, compara o valor e o tipo do dado 
+// != Diferente de
+// !== Diferente e não idêntico, compara o valor e o tipo do dado
+// > Maior que 
+// < Menor que 
+// >= Maior ou igual
+// <= Menor ou igual
+
+var a = 3
+var b = 2
+
+console.log(a == b)
+console.log(a != b)
