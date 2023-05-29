ec08d1af9b68edd7adca9d14c3e9c1bfb75fd9ac
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 16:06:38 -0300

    Operadores Lógicos

---
+++ JavaScript Básico/Operadores Lógicos/script.js
@@ -0,0 +1,8 @@
+// && (and) Compara se os dois elementos são true, se apenas um for false ele retorna false 
+// || (ou) Compara se apenas um dos elementos é true, se apenas um for true ele retorna true
+// !  (Negação) Altera o resultado, se for true ele se torna false, e se for false ele se torna true
+
+var a = true
+var b = true
+
+console.log(a && b)
