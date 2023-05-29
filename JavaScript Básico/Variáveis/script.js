90e942376998e92b3ef9b99c7f738becef7fd68a
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 14:35:29 -0300

    Variáveis com JavaScript

---
+++ JavaScript Básico/Variáveis/script.js
@@ -0,0 +1,17 @@
+var a = 'Usando o valor de uma variável' //Exemplo de uma variável String
+var b = 2.25        //Exemplo de uma variável Number
+var c = true        //Exemplo de uma variável Boolean
+var d = null        //Exemplo de uma variável Null
+var e = undefined   //Exemplo de uma variável undefined
+
+console.log(typeof(b))
+
+/*
+A variável String suporta todos os tipos de caracteres
+A variável Number são todos os números racionais
+A variável Null é uma variável vazia
+A variável Undefined é uma variável sem valor
+A variável Boolean tem apenas duas opções, o true ou false
+
+A diferença entre null e undefined é que null é utilizado para indicar a ausência de um objeto, enquanto undefined indica a aunsência de um valor
+*/
