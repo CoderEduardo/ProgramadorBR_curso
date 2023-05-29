750e7d6be377bb785e88226f67be37f793bfe05f
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 17:28:46 -0300

    Transformando Strings em Números com o parse

---
+++ JavaScript Básico/Transformando Strings em Números/script.js
@@ -0,0 +1,11 @@
+var a = "8"
+var b = "15"
+var c = parseInt(a) + parseInt(b)   //Para transformar Strings em números Inteiros usamos o parseInt(), assim, possibilitando a soma
+
+console.log(c)
+
+a = "51.54"
+b = "92.79"
+c = parseFloat(a) + parseFloat(b)  //Para transformar Strings em número Racionais usamos o parseFloat(), assim, possibilitando a soma
+
+console.log(c)
