8506d2a807cf8ae6a52a1c54b08ea719c22782f8
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 18:54:03 -0300

    Usando Operador Ternário na prática

---
+++ JavaScript Básico/Operador Ternário/script.js
@@ -0,0 +1,7 @@
+//Usando o Operador Ternário
+
+var idade = 17
+
+idade >= 18 ? console.log("Você pode comprar bebida") : console.log("Você não pode comprar")
+
+//var pode = idade >= 18 ? true : false  também podemos usar ele dessa forma, apenas retornando o valor para uma variável e usando ela depois
