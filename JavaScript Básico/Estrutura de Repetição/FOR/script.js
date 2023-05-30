af9aab7e5d40d05315ee5b96eafd00f5632a69ed
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 21:41:04 -0300

    Estrutura de Repetição For

---
+++ JavaScript Básico/Estrutura de Repetição/FOR/script.js
@@ -0,0 +1,5 @@
+//Uma forma básica de usar o for, primeiro você declara a variável, depois adiciona a condição e por último o incremento
+
+for (var i = 0; i <=100; i++ ){
+    document.write(i + "<br>")
+}
