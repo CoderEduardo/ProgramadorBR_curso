328a0d5d2fac71711a94fac945ad6680b86214e2
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-29 22:05:55 -0300

    Estrutura de Repetição While

---
+++ JavaScript Básico/Estrutura de Repetição/WHILE/script.js
@@ -0,0 +1,9 @@
+var numero = Math.random() * 100
+
+while(numero <= 90){
+    document.write(numero + "<br>")
+    numero = Math.random() * 100
+}
+
+document.write(numero + "<br>")
+document.write("Acabou")
