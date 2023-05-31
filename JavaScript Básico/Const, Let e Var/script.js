4316f77f2203ecbb0c1999b0227d313d1f5d88fd
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-30 22:44:40 -0300

    Diferenças entre Const, Let e Var

---
+++ JavaScript Básico/Const, Let e Var/script.js
@@ -0,0 +1,6 @@
+var numero1 = 5
+let numero2 = 10 //O let é igual um var, a única diferença é que o var é visto de um escopo global, já o let é vista apenas de um escopo local
+const numero3 = 15 //O const é uma constante, uma vez que foi atribuido um valor a ele, você não pode alterar
+
+//Entre let e var, o recomendado é usar sempre let, pois ao usar let você não precisa se preocupar se usou o nome para uma variável antes, já que a variável
+//existe somente dentro daqule esocpo
