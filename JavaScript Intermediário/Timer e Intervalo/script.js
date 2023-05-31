2c71a07d268fd63e16abcbc1dd9e10af75dc3c94
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-31 17:14:55 -0300

    Usando setTimeount e setInterval

---
+++ JavaScript Intermediário/Timer e Intervalo/script.js
@@ -0,0 +1,17 @@
+var numero = 0
+var interval
+
+function atualizarTexto(){
+    let h1 = document.getElementsByTagName("h1")[0]
+    h1.innerText += numero 
+    numero++
+}
+
+function iniciar(){
+    interval = setInterval(atualizarTexto,1000)
+}
+
+function parar(){
+    clearInterval(interval)
+}
+
