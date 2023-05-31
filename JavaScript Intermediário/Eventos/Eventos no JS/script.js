e210d2a6d058e08b3dc36c1900f05c93990eddc9
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-31 12:45:13 -0300

    Pequena atualização nos Eventos

--- JavaScript Intermediário/Eventos/Eventos no JS/script.js
+++ JavaScript Intermediário/Eventos/Eventos no JS/script.js
@@ -1,5 +1,6 @@
 let t = document.getElementById("titulo")
 t.addEventListener("click",mudarTexto) 
+//t.click = mudarTexto Também conseguimos fazer dessa forma
 
 function mudarTexto(){
     t.innerText = "O texto foi mudado"
