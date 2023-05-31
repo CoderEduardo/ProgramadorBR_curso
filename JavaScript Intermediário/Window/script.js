253bfffa80ec7dcf015c8bb85c768d2cc6030e47
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-30 23:01:27 -0300

    Entendendo a importância do Window

---
+++ JavaScript Intermediário/Window/script.js
@@ -0,0 +1,13 @@
+/*O objeto window é um objeto padrão do JS, estando em diversos lugares, ele também é um dos mais importantes, e por ser tão importante, você não precisa
+chamar diretamente ele quando for usar alguns de seus métodos, como o alert ou console.log */
+
+window.alert("Olá mundo")
+
+alert("Olá mundo")
+
+//Os dois vão ter o mesmo resultado, isso porque não precisamos chamar o window pelo seu nome, outro exemplo é:
+
+window.console.log("Olá mundo")
+
+console.log("Olá mundo")
+
