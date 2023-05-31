052d3935b086554cc6e1dd3c43653aa58bca4b1e
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-31 13:31:42 -0300

    Usando a propriedada Math para mexer com números

---
+++ JavaScript Intermediário/MathJS/script.js
@@ -0,0 +1,20 @@
+//A biblioteca Math serve para fazermos muitas coisa envolvendo números
+
+//Mostrando o número pi
+let pi = Math.PI
+console.log(pi)
+
+//Calcular a raiz quadrada
+let raizQuadrada = Math.sqrt(16)
+console.log(raizQuadrada)
+
+//Calcular a potência
+let potencia = Math.pow(2, 3)  //dois elevado a três
+console.log(potencia)
+
+let aleatorio = Math.round(Math.random() * 10)       //Math.random() gera um número aleatório de 0 a 0,99
+console.log(aleatorio)                               //Math.round() arredonda o número que está dentro dele
+
+//Math.floor arredonda o número totalmente para baixo
+//Math.ceil arredonda o número totalmente para cima
+//Math.round arredonda o número na fomra padrão, se está até meio ou acima de meio
