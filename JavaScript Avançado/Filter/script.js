0b46759a23d7c366c0fcd474ecba787b4d6bb7a4
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-07 22:42:23 -0300

    Usando o filter para pesquisar itens em um array

---
+++ JavaScript Avançado/Filter/script.js
@@ -0,0 +1,20 @@
+function novoAluno(nome, idade){
+    return {
+        nome, idade
+    }
+}
+
+let alunos = [
+    novoAluno("Luis Eduardo", 18),
+    novoAluno("João", 25),
+    novoAluno("Ana Júlia",89),
+    novoAluno("José",55)
+]
+
+/*Uma das formas de filtrar um Array é passando uma função por callback e usar o .finter*/
+
+function menosDe30(aluno){
+    return aluno.idade < 30
+}
+
+console.log(alunos.filter(menosDe30))
